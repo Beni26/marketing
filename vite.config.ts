0b3c9ee5ae2970cdcc95b03144d7dda1ae185d20
benyamin <benjamin.net1990@gmail.com>
2024-06-30 21:08:46 +0330

    fix prop-types

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   // server: {
+//   //   proxy: {
+//   //     '/api': {
+//   //       target: 'http://178.252.151.68:1407',
+//   //       changeOrigin: true,
+//   //       secure: false,
+//   //     }
+//   //   }
+//   // }
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://178.252.151.68:1407',
-  //       changeOrigin: true,
-  //       secure: false,
-  //     }
-  //   }
-  // }
-})
+  build: {
+    rollupOptions: {
+      external: ['prop-types']
+    }
+  }
+});
